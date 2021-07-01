bbd63cb8e824930c2c7ec791b95dd43eda855e76
Justin Chadwell <me@jedevc.com>
2021-07-01 22:10:16 +0100

    Add delete achievement method

--- api/app/main.py
+++ api/app/main.py
@@ -277,3 +277,25 @@ def read_user_achievement(
     if not db_achievement:
         raise HTTPException(status_code=404, detail="Achievement not found")
     return db_achievement
+
+@app.delete(
+    "/users/{username}/achievements/{achievement}",
+    response_model=schemas.Achievement,
+    tags=["achievements"],
+)
+def delete_user_achievement(
+    username: Union[uuid.UUID, str],
+    achievement: uuid.UUID,
+    db: Session = Depends(get_db),
+    token: models.Token = Depends(get_token_admin),
+):
+    db_user = crud.get_user(db, username)
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    db_achievement = crud.get_user_achievement(db, db_user, achievement)
+    if not db_achievement:
+        raise HTTPException(status_code=404, detail="Achievement not found")
+
+    db.delete(db_achievement)
+    db.commit()
